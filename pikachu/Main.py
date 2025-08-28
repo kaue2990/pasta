2b86015861a79f744c2f5c0ac52324b589536302
Isabelli <eloamenezes2023@gmail.com>
2025-08-28 16:45:49 -0300

    Aula

--- pikachu/Main.py
+++ pikachu/Main.py
@@ -1,22 +1,24 @@
+import random
 def main():
-    notas = [] * 4
+    notas = [0.0] * 4
+    
+    
     i = 0
     while i < 4:
-        notas.append(random.randint(0, 10))
+        notas[i] = random.uniform(0, 10)
+        print("A ", i + 1,  f"ª nota foi de {notas[i]:,.3}")
+        media += notas[i]
         i += 1
-    media = sum(notas) / len(notas)
-    print("Notas:", notas)
-    print("Média:", media)
+
+    media /= i
+    print(f"A media do pikachu foi de {notas[i]:,.3}")
+
     if media >= 6:
-        print("Aprovado!")
+        print("Aprovado, boas férias!")
+    elif media >= 4:
+        print("Recuperação, vai ficar aqui em dezembro Muahahaha!!!")
     else:
-        print("Reprovado!")
-          
-    
-    
-    
-    
-    
-    
+        print("Pelo amor, sai daqui")
+
     return 0
 main()
---
+++ pikachu/par e impar/Main.py
@@ -0,0 +1,12 @@
+def main():
+    num = int (input("Digite um número: "))
+
+    if num % 2 == 0:
+        print("O número ", num, " é par")
+    else:
+        print("O número ", num, " é impar")
+
+
+        
+    return 0
+main()
